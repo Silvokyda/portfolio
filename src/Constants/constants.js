72ecaa5572e72df183808f4f7963366f40d2e031
Silvokyda <deseve48@gmail.com>
2024-04-16 22:08:08 +0300

    constant

--- src/Constants/constants.js
+++ src/Constants/constants.js
@@ -74,7 +74,7 @@ const introduction = {
   text: [
     "Hello there, I'm so happy you are here! (:",
 
-		"My name is Sara, I'm 21 and a junior computer science student based in Guatemala. I have a strong passion for the technologies related to the data base construction and UX.",
+		"My name is SIlvanus, I'm 21. I have a strong passion for the technologies related to the data base construction and UX.",
     "Outside of my regular activities, I'm into drawing whenever I have some free time. I'm also a huge dog lover and enjoy playing chess for fun.",
 
     "I'm continuously thrilled to partner with individuals and teams who wholeheartedly embrace my fervor for crafting extraordinary experiences. Your visit to my portfolio is deeply appreciated !",
